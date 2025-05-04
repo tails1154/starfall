12ec1473b21049b7d0fc55932047e6249a1bc92c
Timothy Heironimus <timothyheironimus6+github@gmail.com>
2025-05-04 12:51:25 -0500

    update script and update camera

--- pushandgalaxy.sh
+++ pushandgalaxy.sh
@@ -1,5 +1,5 @@
 commit_msg=$(kdialog --inputbox "Enter commit message:") && git init && git add . && git commit -m "$commit_msg" && git push || true && cd SMG2starfall 
-kdialog --yesno "Start dolphin?" && QT_QPA_PLATFORM=xcb dolphin-emu -b preset.json & cd .. &
+QT_QPA_PLATFORM=xcb dolphin-emu -b preset.json & cd .. &
 galaxy=$(kdialog --inputbox "Enter galaxy name")
 starno=$(kdialog --inputbox "StarNo")
 scenariono=$(kdialog --inputbox "ScenarioNo")
